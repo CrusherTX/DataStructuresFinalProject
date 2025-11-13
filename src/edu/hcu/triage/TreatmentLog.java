9d5fc2a2243d28673e324aa2b0a3791e4c289bcc
CrusherTX <135666748+CrusherTX@users.noreply.github.com>
2025-11-13 15:32:59 -0600

    Create TreatmentLog.java

---
+++ src/edu/hcu/triage/TreatmentLog.java
@@ -0,0 +1,13 @@
+package edu.hcu.triage;
+
+import java.util.*;
+
+/** Append-only treatment log using LinkedList. */
+public class TreatmentLog {
+    private final LinkedList<TreatedCase> log = new LinkedList<>();
+
+    // TODO: append(TreatedCase tc)
+    // TODO: size()
+    // TODO: asListOldestFirst()
+    // TODO: asListNewestFirst()
+}
